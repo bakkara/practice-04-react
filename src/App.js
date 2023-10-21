450e6c41420d934805400de6bdd4942f2b73d587
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2023-10-21 12:07:23 +0200

    redux done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from "react-router-dom";
 import { lazy, useEffect } from "react";
-import './App.css';
-import Layout from './components/Layout';
-import { userInfo } from './services/getUserInfo';
+import "./App.css";
+import Layout from "./components/Layout";
+import { useDispatch } from "react-redux";
+import { baseCurrencyThunk } from "./redux/operation";
+import { setBaseCurrency } from "./redux/slice";
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const RatesPage = lazy(() => import('./pages/RatesPage'));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const RatesPage = lazy(() => import("./pages/RatesPage"));
 
 function App() {
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (navigator.geolocation)
-    {navigator.geolocation.getCurrentPosition(position => {
-                                                const lat = position?.coords?.latitude;
-                                                const long = position?.coords?.longitude;
-    userInfo({lat, long});
-                                              })
-                                            }
-  }, [])
-  
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    function success(pos) {
+      const crd = pos.coords;
+      dispatch(baseCurrencyThunk(crd));
+    }
+
+    function error(error) {
+      dispatch(setBaseCurrency("USD"));
+      console.warn(`${error.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [dispatch]);
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='/rates' element={<RatesPage/>} />
+          <Route path="/rates" element={<RatesPage />} />
         </Route>
-        <Route path='*' element={<Navigate to='/' />}/>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
--- src/services/getUserInfo.js
+++ src/services/getUserInfo.js
@@ -2,14 +2,17 @@ import axios from "axios";
 
 const API_KEY = "c34efdc8ec4d43dfa0a3980567efbefd";
 
-axios.defaults.baseURL='https://api.opencagedata.com/geocode/v1/json'
+axios.defaults.baseURL = "https://api.opencagedata.com/geocode/v1/json";
 
-export const userInfo = async({lat, long}) => {
+export const userInfo = async (crd) => {
+  console.log(crd);
   try {
-    const {data} = await axios.get(`?q=${lat}+${long}&key=${API_KEY}`);
+    const { data } = await axios.get(
+      `?q=${crd.latitude}+${crd.longitude}&key=${API_KEY}`
+    );
     // console.log(data.results[0].annotations.currency.iso_code);
     return data.results[0].annotations.currency.iso_code;
   } catch (error) {
     console.log(error);
   }
-}
+};
